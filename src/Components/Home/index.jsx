f4ec40f817895c1ad8e9f1d85590e2b7a559604f
Victor-OT <crowbreaker96@hotmail.com>
2024-07-24 14:41:40 -0600

    Se estilizó el componente Home

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,16 +1,20 @@
 import { Container, Row, Col } from "react-bootstrap"
 import { ArrowRightCircle } from "react-bootstrap-icons"
+import './Home.css'
 
 function Home () {
     return (
         <section className="home">
             <Container>
                 <Row className='align-items-center'>
-                    <Col xs={12} md={6} xl={7}>
+                    <Col xs={12} md={6} xl={6} className="main-text-container">
                         <span className="tagline">Mi Portafolio</span>
-                        <h1>{'Hola! Soy Victor Olivares'}<span className="wrap">Frontend Developer</span></h1>
-                        <p>Soy un desarrollador frontend junior con experiencia en JavaScript, HTML5 y CSS. Utilizo React como framework principal y tengo habilidades en Bootstrap y TailwindCSS. Me especializo en diseño responsivo para garantizar una experiencia óptima en todos los dispositivos y pantallas. Busco oportunidades para aplicar mis habilidades y continuar aprendiendo en un entorno dinámico y colaborativo.</p>
+                        <h1>Hola! Soy Victor Olivares, Frontend Developer</h1>
+                        <p>Soy un desarrollador frontend junior con experiencia en JavaScript, HTML5 y CSS. Utilizo React como framework principal y tengo habilidades en Bootstrap y TailwindCSS. Poseo conocimientos en diseño responsivo para garantizar una experiencia óptima en todos los dispositivos y pantallas. Busco oportunidades para aplicar mis habilidades y continuar aprendiendo en un entorno dinámico y colaborativo.</p>
                         <button onClick={() => console.log('Connect')}>Let's connect <ArrowRightCircle size={24}/></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src="../../../public/header-img.svg" alt="" />
                     </Col>
                 </Row>
             </Container>
