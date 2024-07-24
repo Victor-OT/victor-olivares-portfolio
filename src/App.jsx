bb89b538f52236a216aed38af55913bacf277c99
Victor-OT <crowbreaker96@hotmail.com>
2024-07-23 19:31:15 -0600

    Se comenzó a desarrollar componente NavBar con componentes de Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { NavBar } from './Components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
 
   return (
-    <div>Hola</div>
+    <NavBar />
   )
 }
 
