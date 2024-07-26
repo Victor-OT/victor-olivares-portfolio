e082bce1bbae7116633a0fa14c64e60530396606
Victor-OT <crowbreaker96@hotmail.com>
2024-07-26 13:01:22 -0600

    Se modificó el párrafo introductorio, se añadió una imagen de fondo para home, se instaló react-multi-carousel para su futura implementación

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -10,7 +10,7 @@ function Home () {
                     <Col xs={12} md={6} xl={6} className="main-text-container">
                         <span className="tagline">Mi Portafolio</span>
                         <h1>Hola! Soy Victor Olivares, Frontend Developer</h1>
-                        <p>Soy un desarrollador frontend junior con experiencia en JavaScript, HTML5 y CSS. Utilizo React como framework principal y tengo habilidades en Bootstrap y TailwindCSS. Poseo conocimientos en diseño responsivo para garantizar una experiencia óptima en todos los dispositivos y pantallas. Busco oportunidades para aplicar mis habilidades y continuar aprendiendo en un entorno dinámico y colaborativo.</p>
+                        <p>Soy desarrollador frontend, apasionado por la tecnología. Tengo conocimientos en diferentes herramientas de desarrollo que me permiten desenvolverme de manera fluída. Poseo conocimientos en diseño responsivo para garantizar una experiencia óptima en todos los dispositivos y pantallas. Busco oportunidades para aplicar mis habilidades y continuar aprendiendo en un entorno dinámico y colaborativo.</p>
                         <button onClick={() => console.log('Connect')}>Let's connect <ArrowRightCircle size={24}/></button>
                     </Col>
                     <Col xs={12} md={6} xl={6}>
