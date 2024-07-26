9f45bac88e361a0ef65dfa64302901b3cf6a015d
Victor-OT <crowbreaker96@hotmail.com>
2024-07-26 12:33:33 -0600

    Se corrigieron estilos para un diseño responsivo óptimo

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -13,8 +13,8 @@ function Home () {
                         <p>Soy un desarrollador frontend junior con experiencia en JavaScript, HTML5 y CSS. Utilizo React como framework principal y tengo habilidades en Bootstrap y TailwindCSS. Poseo conocimientos en diseño responsivo para garantizar una experiencia óptima en todos los dispositivos y pantallas. Busco oportunidades para aplicar mis habilidades y continuar aprendiendo en un entorno dinámico y colaborativo.</p>
                         <button onClick={() => console.log('Connect')}>Let's connect <ArrowRightCircle size={24}/></button>
                     </Col>
-                    <Col xs={12} md={6} xl={5}>
-                        <img src="/header-img.svg" alt="" />
+                    <Col xs={12} md={6} xl={6}>
+                        <img src="/header-img.svg" alt="" className="header-image"/>
                     </Col>
                 </Row>
             </Container>
