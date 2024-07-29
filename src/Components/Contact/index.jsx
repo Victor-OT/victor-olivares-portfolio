d5872deb96793cc5a30d5bfa4172e84199865703
Victor-OT <crowbreaker96@hotmail.com>
2024-07-29 12:39:15 -0600

    Se desarrolló componente Contact, y se elaboró la lógica del formulario, de momento solo actualiza datos en un array formDetails, no se ha llamado en App

---
+++ src/Components/Contact/index.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react"
+import { Container, Row, Col } from "react-bootstrap"
+
+function Contact () {
+    const formInitialDetails = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+        message: ''
+    }
+
+    const [formDetails, setFormDetails] = useState(formInitialDetails)
+    const [buttonText, setButtonText] = useState('Send')
+    const [status, setStatus] = useState ({})
+
+    const onFormUpdate = (category, value) => {
+        setFormDetails({...formDetails, [category]: value})
+    }
+
+    const handleSubmit = () => {
+        
+    }
+
+    return (
+        <section className="contact" id='connect'>
+            <Container>
+                <Row className="align-items-center">
+                    <Col md={6}>
+                        <img src="/contact-img.svg" alt="contact" />
+                    </Col>
+                    <Col md={6}>
+                        <h2>Get In Touch</h2>
+                        <form onSubmit={() => handleSubmit()}>
+                            <Row>
+                                <Col sm={6}>
+                                    <input type="text" value={formDetails.firstName} placeholder="First Name" 
+                                        onChange={(event) => onFormUpdate('firstName', event.target.value)}/>
+                                </Col>
+                                <Col sm={6}>
+                                    <input type="text" value={formDetails.lastName} placeholder="Last Name" 
+                                        onChange={(event) => onFormUpdate('lastName', event.target.value)}/>
+                                </Col>
+                                <Col sm={6}>
+                                    <input type="email" value={formDetails.email} placeholder="Email Address" 
+                                        onChange={(event) => onFormUpdate('email', event.target.value)}/>
+                                </Col>
+                                <Col sm={6}>
+                                    <input type="tel" value={formDetails.phone} placeholder="Phone No." 
+                                        onChange={(event) => onFormUpdate('phone', event.target.value)}/>
+                                </Col>
+                                <Col>
+                                    <textarea row={6} value={formDetails.message} placeholder="Message" 
+                                        onChange={(event) => onFormUpdate('message', event.target.value)} ></textarea>
+                                    <button type="submit"><span>{buttonText}</span></button>
+                                </Col>
+                                {
+                                    status.message &&
+                                    <Col>
+                                        <p className={status.success === false ? 'danger' : success}>{status.message}</p>
+                                    </Col>
+                                }
+                            </Row>
+                        </form>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
+
+export {Contact}
